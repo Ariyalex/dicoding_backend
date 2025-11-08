7b05bf27045ff23b31cc432d0884bffab4ae1fc9
Ariyalex <ariya17duta@gmail.com>
2025-11-08 21:30:08 +0700

    feat: add owner field to notes and implement authorization checks for note operations

---
+++ notes-app-back-end/migrations/1762609633143_add-column-owner-to-table-notes.js
@@ -0,0 +1,25 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.addColumn("notes", {
+    owner: {
+      type: "varchar(50)",
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropColumn("notes", "owner");
+};
